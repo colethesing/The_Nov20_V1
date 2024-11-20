4b548293fdd88e2eb8703a58d2d96d22d7c81ada
colethesing <colethes@gmail.com>
2024-11-20 13:44:00 -0600

    new Class Student

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Nov20.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? StudentFirstName { get; set; }
+        public String? StudentLastName { get; set; }
+
+    }
+}
